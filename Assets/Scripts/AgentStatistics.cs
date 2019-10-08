771c861e2e2a5a4719b77ec6051d290f44c90a7a
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-10-08 21:32:48 +0200

    Added agent statistics

---
+++ Assets/Scripts/AgentStatistics.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Bloodstone.AI
+{
+    public struct AgentStatistics
+    {
+        [SerializeField]
+        private Vector3 _velocity;
+
+        [SerializeField]
+        private Vector3 _angularVelocity;
+
+        public Vector3 Velocity
+        {
+            get => _velocity;
+            set => _velocity = value;
+        }
+
+        public Vector3 AngularVelocity
+        {
+            get => _angularVelocity;
+            set => _angularVelocity = value;
+        }
+    }
+}
