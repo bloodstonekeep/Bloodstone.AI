97278a421acdaf98997a1225065de9dc83cba915
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-10-09 20:04:37 +0200

    Added Agent class

---
+++ Assets/Bloodstone.AI/Agent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Bloodstone.AI
+{
+    public class Agent : MonoBehaviour
+    {
+        [SerializeField]
+        private AgentStatistics _statistics;
+
+        [SerializeField]
+        private Vector3 _velocity;
+
+        [SerializeField]
+        private Vector3 _angularVelocity;
+
+        public Vector3 Velocity
+        {
+            get => _velocity;
+            set => _velocity = value;
+        }
+
+        public Vector3 AngularVelocity
+        {
+            get => _angularVelocity;
+            set => _angularVelocity = value;
+        }
+
+        public AgentStatistics Statistics => _statistics;
+    }
+}
