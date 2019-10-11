f54b2865067b6be15eda8a828d41a2bd4af139d5
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-10-11 23:30:05 +0200

    Clean up

---
+++ Assets/Bloodstone.AI/KinematicActuator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class KinematicActuator : MonoBehaviour
+{
+    public Quaternion Rotation
+    {
+        get => transform.rotation;
+        set => transform.rotation = value;
+    }
+
+    public Vector3 Position
+    {
+        get => transform.position;
+        set => transform.position = new Vector3(value.x, value.y, 0);
+    }
+
+    private void Update()
+    {
+        Velocity += _flocking.GetVelocity();
+        Velocity = Vector2.ClampMagnitude(Velocity, MaxSpeed);
+        Position += Velocity * Time.deltaTime;
+
+        if (UseAngularVelocity)
+        {
+            AngularVelocity = _flocking.GetAngularVelocity();
+            AngularVelocity = Mathf.Clamp(AngularVelocity, -MaxAngularVelocity, MaxAngularVelocity);
+            Rotation = Quaternion.Euler(0, 0, Rotation.eulerAngles.z + AngularVelocity);
+        }
+        else
+        {
+            Rotation = Quaternion.Euler(0, 0, Mathf.Atan2(Velocity.y, Velocity.x) * Mathf.Rad2Deg);
+        }
+
+        BoundToScreen();
+    }
+}
