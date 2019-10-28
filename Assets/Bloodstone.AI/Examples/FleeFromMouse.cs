95f4a5a5f7ef240cdca6ce0a25a129ed5a27bdb1
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-10-28 20:14:42 +0100

    Added flee from mouse behaviour

---
+++ Assets/Bloodstone.AI/Examples/FleeFromMouse.cs
@@ -0,0 +1,27 @@
+﻿using Bloodstone.AI.Steering;
+using UnityEngine;
+
+namespace Bloodstone.AI.Examples
+{
+    public class FleeFromMouse : Seek
+    {
+        private float _separationRadius = 1;
+
+        public override Vector3 GetSteering()
+        {
+            var camera = Camera.main;
+
+            var mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z -= camera.transform.position.z;
+            TargetPosition = mousePos;
+
+            var dist = mousePos - Agent.Position;
+            if (dist.sqrMagnitude < _separationRadius * _separationRadius)
+            {
+                return -base.GetSteering();
+            }
+
+            return Vector3.zero;
+        }
+    }
+}
