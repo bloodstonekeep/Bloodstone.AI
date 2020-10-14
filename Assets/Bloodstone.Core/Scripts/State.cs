5066367936b4616861d666bb407f79ba336b88b9
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2020-10-14 07:42:21 +0200

    Added poc fsm

---
+++ Assets/Bloodstone.Core/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bloodstone
+{
+    [Serializable]
+    public abstract class State
+    {
+        public abstract void Activate();
+
+        public abstract void Deactivate();
+
+        public abstract void Tick();
+
+        public virtual void LateTick()
+        {
+        }
+
+        public virtual void TickPhysics()
+        {
+        }
+    }
+}
---
+++ Assets/Bloodstone.Core/Scripts/StateMachine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bloodstone
+{
+    [Serializable]
+    public class StateMachine<T> where T : State
+    {
+        [SerializeField]
+        private State _currentState;
+
+        public StateMachine(State initialState)
+        {
+            if (initialState is null)
+            {
+                throw new ArgumentNullException(nameof(initialState));
+            }
+
+            SetState(initialState);
+        }
+
+        public void Tick()
+        {
+            _currentState?.Tick();
+        }
+
+        public void SetState(State newState)
+        {
+            if (_currentState != newState)
+            {
+                _currentState?.Deactivate();
+                _currentState = newState;
+                _currentState.Activate();
+            }
+        }
+    }
+}
